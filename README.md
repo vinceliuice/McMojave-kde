a100cc61177db6d19c57d148ff22ba8aef2ed324
vinceliuice <vinceliuice@hotmail.com>
2019-05-11 19:53:22 +0800

    upload

---
+++ README.md
@@ -0,0 +1,38 @@
+<img src="https://github.com/vinceliuice/Qogir-kde/blob/master/images/logo.png" alt="Logo" align="right" /> Qogir KDE Theme
+======
+
+McMojave kde is a MacOSX Mojave like theme for KDE Plasma desktop.
+
+In this repository you'll find:
+
+- Aurorae Themes
+- Kvantum Themes
+- Plasma Color Schemes
+- Plasma Desktop Themes
+- Plasma Look-and-Feel Settings
+
+## Installation
+
+```sh
+./install.sh
+```
+
+## Recommendations
+
+- For better looking please use this pack with [Kvantum engine](https://github.com/tsujan/Kvantum/tree/master/Kvantum).
+
+  Run `kvantummanager` to choose and apply **Qogir-light** (or any other Qogir) theme.
+
+- Install [Qogir icon theme](https://github.com/vinceliuice/Qogir-icon-theme) for a more consistent and beautiful experience.
+
+## Donate
+
+If you like my project, you can donate at:
+
+<span class="paypal"><a href="https://www.paypal.me/vinceliuice" title="Donate to this project using Paypal"><img src="https://www.paypalobjects.com/webstatic/mktg/Logo/pp-logo-100px.png" alt="PayPal donate button" /></a></span>
+
+## License
+
+GNU GPL v3
+
+
---
+++ install.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+SRC_DIR=$(cd $(dirname $0) && pwd)
+
+AURORAE_DIR="$HOME/.local/share/aurorae/themes"
+SCHEMES_DIR="$HOME/.local/share/color-schemes"
+PLASMA_DIR="$HOME/.local/share/plasma/desktoptheme"
+LOOKFEEL_DIR="$HOME/.local/share/plasma/look-and-feel"
+KVANTUM_DIR="$HOME/.config/Kvantum"
+
+THEME_NAME=McMojave
+COLOR_VARIANTS=('' '-light')
+
+install() {
+  local name=${1}
+  local color=${2}
+
+  local AURORAE_THEME=${AURPRAE_DIR}/${name}${color}
+  local PLASMA_THEME=${PLASMA_DIR}/${name}${color}
+  local LOOKFEEL_THEME=${LOOKFEEL_DIR}/${name}${color}
+  local SCHEMES_THEME=${SCHEMES_DIR}/${name}${color}.colors
+  local KVANTUM_THEME=${KVANTUM_DIR}/${name}-dark
+
+  [[ -d ${AURORAE_THEME} ]] && rm -rf ${AURORAE_THEME}
+  [[ -d ${PLASMA_THEME} ]] && rm -rf ${PLASMA_THEME}
+  [[ -d ${LOOKFEEL_THEME} ]] && rm -rf ${LOOKFEEL_THEME}
+  [[ -d ${SCHEMES_THEME} ]] && rm -rf ${SCHEMES_THEME}
+  [[ -d ${KVANTUM_THEME} ]] && rm -rf ${KVANTUM_THEME}
+
+  cp -ur ${SRC_DIR}/aurorae/themes/${name}${color}                                   ${AURORAE_DIR}
+  cp -ur ${SRC_DIR}/color-schemes/${name}${color}.colors                             ${SCHEMES_DIR}
+  cp -ur ${SRC_DIR}/Kvantum/${name}-dark                                             ${KVANTUM_DIR}
+  cp -ur ${SRC_DIR}/plasma/desktoptheme/${name}${color}                              ${PLASMA_DIR}
+  cp -ur ${SRC_DIR}/color-schemes/${name}${color}.colors                              ${PLASMA_DIR}/${name}${color}/colors
+  cp -ur ${SRC_DIR}/plasma/look-and-feel/com.github.vinceliuice.${name}${color}      ${LOOKFEEL_DIR}
+}
+
+echo "Installing 'McMojave kde themes'..."
+
+for color in "${colors[@]:-${COLOR_VARIANTS[@]}}"; do
+  install "${name:-${THEME_NAME}}" "${color}"
+done
+
+echo "Install finished..."
