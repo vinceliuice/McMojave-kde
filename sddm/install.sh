7259039386ac8763fdc5bded2275e3656afe227f
vinceliuice <vinceliuice@hotmail.com>
2020-01-27 20:40:34 +0800

    update sddm theme

--- sddm/install.sh
+++ sddm/install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-cp -ur McMojave /usr/share/sddm/themes
-
-echo "Install finished..."
+if [[ "$EUID" != 0 ]]; then
+  echo "Please run as root"
+  exit
+else
+  echo "Installing theme..."
+  cp -r McMojave /usr/share/sddm/themes
+  echo "Install finished..."
+fi
