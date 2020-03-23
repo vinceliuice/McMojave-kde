5f617756408f8c954c8370aaf4a56e6c008fc839
vinceliuice <vinceliuice@hotmail.com>
2020-03-23 11:40:48 +0800

    update

--- install.sh
+++ install.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
-SRC_DIR=$(cd $(dirname $0) && pwd)
+ROOT_UID=0
+DEST_DIR=
 
-AURORAE_DIR="$HOME/.local/share/aurorae/themes"
-SCHEMES_DIR="$HOME/.local/share/color-schemes"
-PLASMA_DIR="$HOME/.local/share/plasma/desktoptheme"
-LOOKFEEL_DIR="$HOME/.local/share/plasma/look-and-feel"
-KVANTUM_DIR="$HOME/.config/Kvantum"
+# Destination directory
+if [ "$UID" -eq "$ROOT_UID" ]; then
+  AURORAE_DIR="/usr/share/aurorae/themes"
+  SCHEMES_DIR="/usr/share/color-schemes"
+  PLASMA_DIR="/usr/share/plasma/desktoptheme"
+  LOOKFEEL_DIR="/usr/share/plasma/look-and-feel"
+  LAYOUT_DIR="/usr/share/plasma/layout-templates"
+  KVANTUM_DIR="/usr/share/Kvantum"
+else
+  AURORAE_DIR="$HOME/.local/share/aurorae/themes"
+  SCHEMES_DIR="$HOME/.local/share/color-schemes"
+  PLASMA_DIR="$HOME/.local/share/plasma/desktoptheme"
+  LOOKFEEL_DIR="$HOME/.local/share/plasma/look-and-feel"
+  LAYOUT_DIR="$HOME/.local/share/plasma/layout-templates"
+  KVANTUM_DIR="$HOME/.config/Kvantum"
+fi
+
+SRC_DIR=$(cd $(dirname $0) && pwd)
 
 THEME_NAME=McMojave
 COLOR_VARIANTS=('' '-light')
@@ -15,27 +29,16 @@ install() {
   local name=${1}
   local color=${2}
 
-  local AURORAE_THEME="${AURORAE_DIR}/${name}${color}"
-  local PLASMA_THEME="${PLASMA_DIR}/${name}${color}"
-  local LOOKFEEL_THEME="${LOOKFEEL_DIR}/com.github.vinceliuice.${name}${color}"
-  local KVANTUM_THEME="${KVANTUM_DIR}/${name}${color}"
-
   mkdir -p                                                                           ${AURORAE_DIR}
   mkdir -p                                                                           ${SCHEMES_DIR}
   mkdir -p                                                                           ${PLASMA_DIR}
   mkdir -p                                                                           ${LOOKFEEL_DIR}
   mkdir -p                                                                           ${KVANTUM_DIR}
 
-  [[ -d ${AURORAE_THEME} ]] && rm -r ${AURORAE_THEME}
-  [[ -d ${PLASMA_THEME} ]] && rm -r ${PLASMA_THEME}
-  [[ -d ${LOOKFEEL_THEME} ]] && rm -r ${LOOKFEEL_THEME}
-  [[ -d ${SCHEMES_DIR}/McMojave.colors ]] && rm -r ${SCHEMES_DIR}/McMojave.colors
-  [[ -d ${SCHEMES_DIR}/McMojaveLight.colors ]] && rm -r ${SCHEMES_DIR}/McMojaveLight.colors
-  [[ -d ${KVANTUM_THEME} ]] && rm -rf ${KVANTUM_THEME}
-
   cp -r ${SRC_DIR}/aurorae/${name}*                                                  ${AURORAE_DIR}
   cp -r ${SRC_DIR}/Kvantum/${name}${color}                                           ${KVANTUM_DIR}
   cp -r ${SRC_DIR}/plasma/desktoptheme/${name}${color}                               ${PLASMA_DIR}
+  cp -r ${SRC_DIR}/plasma/layout-templates/org.github.desktop.McMojavePanel          ${LAYOUT_DIR}
   cp -r ${SRC_DIR}/plasma/look-and-feel/com.github.vinceliuice.${name}${color}       ${LOOKFEEL_DIR}
 
   [[ ${color} == '' ]] && \
---
+++ uninstall.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+ROOT_UID=0
+DEST_DIR=
+
+# Destination directory
+if [ "$UID" -eq "$ROOT_UID" ]; then
+  AURORAE_DIR="/usr/share/aurorae/themes"
+  SCHEMES_DIR="/usr/share/color-schemes"
+  PLASMA_DIR="/usr/share/plasma/desktoptheme"
+  LOOKFEEL_DIR="/usr/share/plasma/look-and-feel"
+  LAYOUT_DIR="/usr/share/plasma/layout-templates"
+  KVANTUM_DIR="/usr/share/Kvantum"
+else
+  AURORAE_DIR="$HOME/.local/share/aurorae/themes"
+  SCHEMES_DIR="$HOME/.local/share/color-schemes"
+  PLASMA_DIR="$HOME/.local/share/plasma/desktoptheme"
+  LOOKFEEL_DIR="$HOME/.local/share/plasma/look-and-feel"
+  LAYOUT_DIR="$HOME/.local/share/plasma/layout-templates"
+  KVANTUM_DIR="$HOME/.config/Kvantum"
+fi
+
+SRC_DIR=$(cd $(dirname $0) && pwd)
+
+THEME_NAME=McMojave
+COLOR_VARIANTS=('' '-light')
+
+uninstall() {
+  local name=${1}
+  local color=${2}
+
+  local AURORAE_THEME="${AURORAE_DIR}/${name}${color}"
+  local PLASMA_THEME="${PLASMA_DIR}/${name}${color}"
+  local KVANTUM_THEME="${KVANTUM_DIR}/${name}${color}"
+  local LOOKFEEL_THEME="${LOOKFEEL_DIR}/com.github.vinceliuice.${name}${color}"
+
+  [[ -d ${AURORAE_THEME} ]] && rm -rfv ${AURORAE_THEME}
+  [[ -d ${PLASMA_THEME} ]] && rm -rfv ${PLASMA_THEME}
+  [[ -d ${LOOKFEEL_THEME} ]] && rm -rfv ${LOOKFEEL_THEME}
+  [[ -d ${KVANTUM_THEME} ]] && rm -rfv ${KVANTUM_THEME}
+}
+
+echo "Uninstalling '${THEME_NAME} kde themes'..."
+
+for color in "${colors[@]:-${COLOR_VARIANTS[@]}}"; do
+  uninstall "${name:-${THEME_NAME}}" "${color}"
+done
+
+[[ -d ${LAYOUT_DIR}/org.github.desktop.McMojavePanel ]] && rm -rfv ${LAYOUT_DIR}/org.github.desktop.McMojavePanel
+[[ -d ${AURORAE_DIR}/McMojave-1.5x ]] && rm -rfv ${AURORAE_DIR}/McMojave-1.5x
+[[ -d ${AURORAE_DIR}/McMojave-2.0x ]] && rm -rfv ${AURORAE_DIR}/McMojave-2.0x
+[[ -f ${SCHEMES_DIR}/McMojave.colors ]] && rm -rfv ${SCHEMES_DIR}/McMojave.colors
+[[ -f ${SCHEMES_DIR}/McMojaveLight.colors ]] && rm -rfv ${SCHEMES_DIR}/McMojaveLight.colors
+
+echo "Uninstall finished..."
